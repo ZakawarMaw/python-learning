be12096a14d3603309f1b2601315bc86f767ac89
Zakawar <thihathurazakawarmaung@gmail.com>
2021-07-25 23:08:59 +0630

    -

---
+++ if_else.py
@@ -0,0 +1,14 @@
+# age=int(input('age : '));
+# if age < 18 :
+#   print('You are young')
+# elif age>18 and age<30 :
+#     print('You are normal age');
+# else:
+#     print('You are old');  
+
+tired=input('are you tired? ? "y/n" ');
+if tired=='y' :
+    print('rest well')
+elif tired=='n' :
+     print('go back to work');
+else: print('enter yes or no');    
