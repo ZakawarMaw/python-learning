8a8c81de04f9f8907adb414223dc36ae0604ae9b
Zakawar <thihathurazakawarmaung@gmail.com>
2021-07-25 21:34:50 +0630

    -

---
+++ input.py
@@ -0,0 +1,11 @@
+# print("Hello World");
+
+# name=input(' Name :');
+# print('Hello ' +name);
+
+
+# Calculate Area ( Formula Pi r**2)
+PI=3.142;
+r=int(input('radius : '));
+area=PI*r**2;
+print(area);
