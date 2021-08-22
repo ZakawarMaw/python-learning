9a5a1b6f18058757bea569bc4844f18af4b21563
Zakawar <thihathurazakawarmaung@gmail.com>
2021-08-22 16:42:54 +0630

    -

---
+++ Car/function.py
@@ -0,0 +1,5 @@
+
+def checkEngine() :
+    print('Engine is good')
+
+    
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-from car import Car
+from Car.car import Car  #call class Name
+from Car.function import checkEngine
 marcedes=Car("Marcedes",4);
 marcedes.common();
+
+checkEngine();
